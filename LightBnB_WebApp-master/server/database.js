fca6c5ba792eb128029e339e9422999b65aafdbc
karolinaCodes <karolinaswislocki@hotmail.com>
2021-11-24 23:00:25 +0000

    add getAllReservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -80,7 +80,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT *
+  FROM properties
+  JOIN reservations ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id= $1 AND reservations.start_date > Now()::date
+  LIMIT $2`;
+
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then(result => result.rows)
+    .catch(err => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
