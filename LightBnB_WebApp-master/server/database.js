4dc7760cae21f0af08fc9806a48ba9e831ede6f5
karolinaCodes <karolinaswislocki@hotmail.com>
2021-11-25 02:33:08 +0000

    add query for avg rating in getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -81,11 +81,13 @@ exports.addUser = addUser;
  */
 const getAllReservations = function (guest_id, limit = 10) {
   const queryString = `
-  SELECT *
+  SELECT properties.*, reservations.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN reservations ON properties.id = reservations.property_id
   JOIN property_reviews ON properties.id = property_reviews.property_id
   WHERE reservations.guest_id= $1 AND reservations.start_date > Now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY start_date
   LIMIT $2`;
 
   return pool
